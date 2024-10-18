a7545245adccb1689a261954028ac1f8f579a5e0
hallo35 <125467618+hallo35@users.noreply.github.com>
2024-10-18 16:14:52 +0200

    18.10

--- exercise-002/main.cpp
+++ exercise-002/main.cpp
@@ -1,8 +1,27 @@
 #include <fmt/chrono.h>
 #include <fmt/format.h>
 
-
+/*BSS Segment*/
 int foo;
+
+/*Konstante Daten .rodata Segment*/
+const int rodata = 45;
+
+void doSomething(int value){
+    value = value + 1;
+    fmt::print("doSomething, value {}\n", value);
+}
+
+void doSomethingPTR(int* ptr){
+    *ptr = *ptr + 1;
+    fmt::print("doSomething, value {}\n", *ptr);
+}
+
+int* doReturnAPTR() {
+    int value = 4711;
+    fmt::print("doReturnAPTR, value {}\n", value);  // Klammerfehlstellung korrigiert
+    return &value;  // Vorsicht: Rückgabe eines Zeigers auf eine lokale Variable führt zu undefiniertem Verhalten
+}
 
 auto main(int argc, char **argv) -> int
 {
@@ -14,8 +33,16 @@ auto main(int argc, char **argv) -> int
      */
     fmt::print("Hello, {}!\n", argv[0]);
     fmt::print("Value of foo {} address of foo {}\n", foo, fmt::ptr(&foo));
+    /*Data Segment (initialisierte Daten)*/
+    int bar = 42;
+    doSomething(bar);
+    fmt::print("The value of bar is: {}\n", bar);
+    doSomethingPTR(&bar);
+    fmt::print("The value of bar is: {}\n", bar);
 
+    int* returnedPTR = doReturnAPTR();
 
+    fmt::print("The value of the returned PTR is : {}\n", *returnedPTR);
 
     return 0; /* exit gracefully*/
 }
